cacfe6f3b39d49f79dd23cf395903d40d0a37f78
Bruno Tatsuya <brunomasunaga@gmail.com>
2022-03-07 00:11:48 -0300

    Changed topbar background color and added bottom border

--- src/topbar/topbar.tsx
+++ src/topbar/topbar.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Topbar: React.FC = () => {
   return (
-    <div className="w-screen bg-zinc-800 p-2">
-      <a className="font-extrabold text-sm p-4 hover:bg-zinc-900 text-slate-100 transition-all" href="/">
+    <div className="w-screen bg-zinc-900 p-3 border-b border-zinc-800">
+      <a className="font-extrabold text-sm p-3 hover:bg-zinc-800 text-slate-100 transition-all" href="/">
         <span className="inline-block align-middle">REST TESTER</span>
       </a>
     </div>
