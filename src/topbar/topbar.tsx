421a4102b3b0630a2b81682a261553e290202e9b
Bruno Tatsuya <brunomasunaga@gmail.com>
2022-03-06 23:58:12 -0300

    Created topbar component

---
+++ src/topbar/topbar.tsx
@@ -0,0 +1,11 @@
+function Topbar() {
+  return (
+    <div className="w-screen bg-zinc-800 p-2">
+      <a className="font-extrabold text-sm p-4 hover:bg-zinc-900 text-slate-100 transition-all" href="/">
+        <span className="inline-block align-middle">REST TESTER</span>
+      </a>
+    </div>
+  )
+}
+
+export default Topbar;
