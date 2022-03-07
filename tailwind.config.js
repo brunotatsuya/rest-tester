c762e9b0ed5717197529bbe1b07cd04330784c88
Bruno Tatsuya <brunomasunaga@gmail.com>
2022-03-06 23:55:44 -0300

    Added tailwindcss to project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{tsx,jsx,ts,js}',
+  ],
+  theme: {
+    extend: {
+        fontFamily: {
+            normal: ['Montserrat'],
+        },
+    },
+},
+ // ... 
+}
