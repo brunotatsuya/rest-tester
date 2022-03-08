b7ee2fb2e7e1fb3804888d63a5eea6967d3061c8
Bruno Tatsuya <brunomasunaga@gmail.com>
2022-03-07 22:40:41 -0300

    Added console component into app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-
 import Topbar from "./topbar/topbar";
+import Console from "./console/console";
 
 const App: React.FC = () => {
   return (
     <div className="h-screen bg-zinc-900">
       <Topbar></Topbar>
+      <Console></Console>
     </div>
   );
 }
