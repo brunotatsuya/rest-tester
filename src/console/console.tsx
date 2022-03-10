d00443b6a07aae7051627a01f35dc568ff14bd8a
Bruno Tatsuya <brunomasunaga@gmail.com>
2022-03-10 00:54:31 -0300

    Added console nav tabs structure

--- src/console/console.tsx
+++ src/console/console.tsx
@@ -1,6 +1,19 @@
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+
 import React from "react";
 
 const Console: React.FC = () => {
+
+  const [value, setValue] = React.useState('1');
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
+
   return (
     <>
       <div className="grid grid-cols-12 mt-6">
@@ -14,7 +27,7 @@ const Console: React.FC = () => {
           </select>
         </div>
         <div className="col-span-7 md:col-span-8 lg:col-span-10 pl-8 bg-zinc-800 text-white">
-          <input className="h-12 w-full bg-zinc-800 text-sm outline-none" placeholder="URL"></input>
+          <input className="h-12 w-full bg-zinc-800 text-sm outline-none" placeholder="https://your.amazing/test"></input>
         </div>
         <div className="grid justify-items-center col-span-2 md:col-span-2 lg:col-span-1 bg-zinc-900">
           <button className="h-full w-5/6 font-bold text-white bg-orange-500 hover:bg-orange-600 transition-colors rounded-md">
@@ -22,6 +35,31 @@ const Console: React.FC = () => {
           </button>
         </div>
       </div>
+
+      <div className="grid grid-cols-12 mt-6 gap-4">
+        <div className="col-span-12 md:col-span-6 h-96 bg-zinc-800">
+          <div className="border-b border-gray-200 dark:border-gray-700">
+            <Box sx={{ width: '100%', typography: 'body1' }}>
+              <TabContext value={value}>
+                <Box sx={{ borderBottom: 1, borderColor: 'divider' }} className="text-white">
+                  <TabList onChange={handleChange} aria-label="lab API tabs example">
+                    <Tab label="Parameters" value="1" />
+                    <Tab label="Body" value="2" />
+                    <Tab label="Headers" value="3" />
+                    <Tab label="Authorization" value="4" />
+                  </TabList>
+                </Box>
+                <TabPanel value="1">Item One</TabPanel>
+                <TabPanel value="2">Item Two</TabPanel>
+                <TabPanel value="3">Item Three</TabPanel>
+                <TabPanel value="4">Item Four</TabPanel>
+              </TabContext>
+            </Box>
+
+          </div></div>
+        <div className="col-span-12 md:col-span-6 h-96 bg-zinc-800"></div>
+      </div>
+
     </>
   )
 }
