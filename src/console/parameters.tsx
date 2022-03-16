ab736bd8843de89f826d5d94e80e39828e94718b
Bruno Tatsuya <brunomasunaga@gmail.com>
2022-03-16 00:11:02 -0300

    Minor adjust of bottom spacing

--- src/console/parameters.tsx
+++ src/console/parameters.tsx
@@ -8,7 +8,7 @@ const Parameters: React.FC = () => {
     <>
     {params.map((element, index) => {
       return (
-        <div className="grid grid-cols-2 gap-1 py-1" key={index}>
+        <div className="grid grid-cols-2 gap-1 pb-1" key={index}>
           <div className="col-span-1" >
             <input className=" w-full bg-zinc-700 text-gray-200 py-2 pl-2 focus:outline-none" type="text" placeholder="Key"/>
           </div>
