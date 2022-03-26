63c0236bc7fa3c21a4b0f30615226c456fc74d17
Bruno Tatsuya <brunomasunaga@gmail.com>
2022-03-25 22:48:36 -0300

    Redesigned topbar

--- src/topbar/topbar.tsx
+++ src/topbar/topbar.tsx
@@ -2,12 +2,22 @@ import React from "react";
 
 const Topbar: React.FC = () => {
   return (
-    <div className="w-screen bg-zinc-900 p-3 border-b border-zinc-800">
-      <a className="font-extrabold text-sm p-3 hover:bg-zinc-800 text-slate-100 transition-all" href="/">
-        <span className="inline-block align-middle">REST TESTER</span>
-      </a>
+    <div className="flex items-center justify-between px-4 py-2 bg-zinc-800">
+      <div>
+        <a href="/" className="flex items-center">
+          <img
+            className="h-8 w-8 mr-3"
+            src="../src/favicon.svg"
+            alt="Workflow"
+          />
+          <span className="text-white font-semibold text-lg tracking-tight">
+            REST TESTER
+          </span>
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
+
 
 export default Topbar;
