f6c21208322a175970c331f2ed51ea271ef3d04d
Bruno Tatsuya <brunomasunaga@gmail.com>
2022-03-25 23:41:17 -0300

    Added handlers to control state actions of adding, editing and deleting parameters

--- src/console/parameters.tsx
+++ src/console/parameters.tsx
@@ -1,21 +1,55 @@
 import React from 'react';
 
+interface Parameter {
+  key?: string;
+  value?: string;
+}
 
 const Parameters: React.FC = () => {
-  const [params, setParams] = React.useState([{ key: 'A', value: 'B' },{ key: 'A', value: 'B' }]);
+
+  const [params, setParams] = React.useState(Array<Parameter>());
+
+  const handleAddParameterClick = () => {
+    setParams(params.concat([{ key: '', value: '' }]));
+  };
+
+  const handleParameterChange = (index: number, key?: string, value?: string) => {
+    const newParams = params.map((param, i) => {
+      if (i === index) {
+        return { key: key ? key : param.key, value: value ? value : param.value };
+      } else {
+        return param;
+      }
+    });
+    setParams(newParams);
+  };
+
+  const handleDeleteParameterClick = (index: number) => {
+    const newParams = params.slice();
+    newParams.splice(index, 1);
+    setParams(newParams);
+  };
 
   return (
     <>
-    {params.map((element, index) => {
-      return (
-        <div className="grid grid-cols-2 gap-1 pb-1" key={index}>
-          <div className="col-span-1" >
-            <input className=" w-full bg-zinc-700 text-gray-200 py-2 pl-2 focus:outline-none" type="text" placeholder="Key"/>
+      <button className="h-full w-10 font-bold text-white bg-orange-500 hover:bg-orange-600 transition-colors rounded-md" onClick={handleAddParameterClick} >
+        Add Parameter
+      </button>
+      {params.map((element, index) => {
+        return (
+          <div className="grid grid-cols-10 gap-1 pb-1" key={index}>
+            <div className="col-span-4" >
+              <input className=" w-full bg-zinc-700 text-gray-200 py-2 pl-2 focus:outline-none" type="text" placeholder="Key" value={element.key} onChange={(e) => handleParameterChange(index, e.target.value)} />
+            </div>
+            <div className="col-span-4">
+              <input className=" w-full bg-zinc-700 text-gray-200 py-2 pl-2 focus:outline-none" type="text" placeholder="Value" value={element.value} onChange={(e) => handleParameterChange(index, undefined, e.target.value)} />
+            </div>
+            <div className="col-span-2">
+              <button className="h-full w-full font-bold text-white bg-red-500 hover:bg-red-600 transition-colors rounded-md" onClick={() => handleDeleteParameterClick(index)}>
+                Delete
+              </button>
+            </div>
           </div>
-          <div className="col-span-1">
-            <input className=" w-full bg-zinc-700 text-gray-200 py-2 pl-2 focus:outline-none" type="text" placeholder="Value"/>
-          </div>
-        </div>
         )
       })}
     </>
